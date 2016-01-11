00be771ccd25719aa40f500cc6611e3e7a1bc0db
Mariusz Angielski <kontakt@mariuszangielski.pl>
2016-01-11 20:40:02 +0100

    v2

--- RelayBoardClass.php
+++ RelayBoardClass.php
@@ -13,25 +13,25 @@ class relayBoard {
 	protected $_handler;
 	
 	protected $portOn = [
-						1 => [0x00, 0x28],
-						2 => [0x01, 0x76],
-						3 => [0x02, 0x94],
-						4 => [0x03, 0xca],
-						5 => [0x04, 0x49],
-						6 => [0x05, 0x17],
-						7 => [0x06, 0xf5],
-						8 => [0x07, 0xab]
+						1 => 0x28,
+						2 => 0x76,
+						3 => 0x94,
+						4 => 0xca,
+						5 => 0x49,
+						6 => 0x17,
+						7 => 0xf5,
+						8 => 0xab
 	];
 	
 	protected $portOff = [ 
-						1 => [0x00, 0x9a],
-						2 => [0x01, 0xc4],
-						3 => [0x02, 0x26],
-						4 => [0x03, 0x78],
-						5 => [0x04, 0xfb],
-						6 => [0x05, 0xa5],
-						7 => [0x06, 0x47],
-						8 => [0x07, 0x19]
+						1 =>  0x9a,
+						2 =>  0xc4,
+						3 =>  0x26,
+						4 =>  0x78,
+						5 =>  0xfb,
+						6 =>  0xa5,
+						7 =>  0x47,
+						8 =>  0x19
 	];
 	
 	public function __construct($port) 
@@ -48,18 +48,12 @@ class relayBoard {
 	
 	public function on($port) 
 	{
-		fputs($this->_handler,pack('C*', 0x55, 0x01, 0x4f)); 
-		foreach($this->portOn[$port] as $hData) {
-			fputs($this->_handler,pack('C*', $hData));
-		}
+		fputs($this->_handler,pack('C*', 0x55, 0x01, 0x4f, ($port-1), $this->portOn[$port])); 
 	}
 	
 	public function off($port)
 	{
-		fputs($this->_handler,pack('C*', 0x55, 0x01, 0x46)); 
-		foreach($this->portOff[$port] as $hData) {
-			fputs($this->_handler,pack('C*', $hData));
-		}
+		fputs($this->_handler,pack('C*', 0x55, 0x01, 0x46, ($port-1), $this->portOff[$port])); 
 	}
 
 }
